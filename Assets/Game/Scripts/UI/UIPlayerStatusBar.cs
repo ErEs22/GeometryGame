6c0bb28dadf11170bb0f7fb09d34e1ce6c5d5a2a
Eyre <ere0506@qq.com>
2024-09-10 16:15:44 +0800

    所有UI样式调整完成

--- Assets/Game/Scripts/UI/UIPlayerStatusBar.cs
+++ Assets/Game/Scripts/UI/UIPlayerStatusBar.cs
@@ -8,17 +8,17 @@ using UnityEngine.UI;
 public class UIPlayerStatusBar : UIBase
 {
     //------------Path
-    private string path_Image_Front_Health = "HPBar/FrontBar";
-    private string path_Image_Front_Exp = "ExpBar/FrontBar";
-    private string path_Text_Health = "HPBar/Text_HPValue";
-    private string path_Text_Exp = "ExpBar/Text_ExpValue";
+    private string path_Image_Front_Health = "Status/HPBar/Img_Background/FrontBar";
+    private string path_Image_Front_Exp = "Status/ExpBar/Img_Background/FrontBar";
+    private string path_Image_PlayerIcon = "Status/PlayerIcon/Img_PlayerIcon";
+    private string path_Text_Exp = "Status/Text_ExpValue";
     private string path_Text_CoinCount = "Coin/Text_CoinCount";
     private string path_Text_BonusCoinCount = "BonusCoin/Text_BonusCoinCount";
     //------------
 
     private Image image_Front_Health;
     private Image image_Front_Exp;
-    private TextMeshProUGUI text_HealthValue;
+    private Image image_playerIcon;
     private TextMeshProUGUI text_ExpValue;
     private TextMeshProUGUI text_CoinCount;
     private TextMeshProUGUI text_BonusCoinCount;
@@ -26,7 +26,7 @@ public class UIPlayerStatusBar : UIBase
     private void Awake() {
         image_Front_Health = transform.Find(path_Image_Front_Health).GetComponent<Image>();
         image_Front_Exp = transform.Find(path_Image_Front_Exp).GetComponent<Image>();
-        text_HealthValue = transform.Find(path_Text_Health).GetComponent<TextMeshProUGUI>();
+        image_playerIcon = transform.Find(path_Image_PlayerIcon).GetComponent<Image>();
         text_ExpValue = transform.Find(path_Text_Exp).GetComponent<TextMeshProUGUI>();
         text_CoinCount = transform.Find(path_Text_CoinCount).GetComponent<TextMeshProUGUI>();
         text_BonusCoinCount = transform.Find(path_Text_BonusCoinCount).GetComponent<TextMeshProUGUI>();
@@ -40,6 +40,7 @@ public class UIPlayerStatusBar : UIBase
         EventManager.instance.onExpBarFlash += ExpBarFlash;
         EventManager.instance.onUpdateCoinCount += UpdateCoinCount;
         EventManager.instance.onUpdateBonusCoinCount += UpdateBonusCoinCount;
+        EventManager.instance.onSetCharacterData += SetCharacterIcon;
     }
 
     private void OnDisable() {
@@ -50,6 +51,7 @@ public class UIPlayerStatusBar : UIBase
         EventManager.instance.onExpBarFlash -= ExpBarFlash;
         EventManager.instance.onUpdateCoinCount -= UpdateCoinCount;
         EventManager.instance.onUpdateBonusCoinCount -= UpdateBonusCoinCount;
+        EventManager.instance.onSetCharacterData -= SetCharacterIcon;
     }
 
     public override void InitUI()
@@ -57,25 +59,28 @@ public class UIPlayerStatusBar : UIBase
         uiID = eUIID.PlayerStatusBar;
     }
 
+    private void SetCharacterIcon(CharacterData_SO data)
+    {
+        image_playerIcon.sprite = data.characterIcon;
+    }
+
     private void InitStatusBar(int maxHP)
     {
         image_Front_Health.fillAmount = 1;
         image_Front_Exp.fillAmount = 0;
-        text_HealthValue.text = maxHP + "/" + maxHP;
-        text_ExpValue.text = "Lv.1";
+        text_ExpValue.text = "LV.1";
         text_CoinCount.text = "0";
         text_BonusCoinCount.text = "0";
     }
 
     private void UpdateHealthBar(int currentHP,int maxHP)
     {
-        text_HealthValue.text = currentHP + "/" + maxHP;
         image_Front_Health.fillAmount = EyreUtility.Divide(currentHP,maxHP);
     }
 
     private void UpdateExpBar(int currentPlayerLevel,int currentExp,int currentLevelMaxExp)
     {
-        text_ExpValue.text = "Lv." + currentPlayerLevel;
+        text_ExpValue.text = "LV." + currentPlayerLevel;
         image_Front_Exp.fillAmount = EyreUtility.Divide(currentExp,currentLevelMaxExp);
     }
 
